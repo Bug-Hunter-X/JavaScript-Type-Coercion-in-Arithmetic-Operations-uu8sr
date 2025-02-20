72f05c22164df37e76039ba53862060227b27ebe
BugHunter-X <askzenai@gmail.com>
2025-02-20 19:14:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,4 @@
+function foo(a,b){return a+b;}console.log(foo(1, "1")); // Output: 11
+function foo(a,b){return a-b;}console.log(foo(1, "1")); // Output: 0
+function foo(a,b){return a*b;}console.log(foo(1, "1")); // Output: 1
+function foo(a,b){return a/b;}console.log(foo(1, "1")); // Output: 1
